5cd991f4851d38329bd730d656c6001150b3ce18
Walter Herold Veedla <walterheroldv@gmail.com>
2021-05-24 17:16:30 +0200

    Preprocessor skeleton added. Added test media.

--- Gui.py
+++ Gui.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from tkinter import filedialog
 
+
 class Gui:
-    """Contains methods related to GUI creation and file selection
+    """
+    Contains methods related to GUI creation and file selection
     """
 
-    def createAndShowGUI(self):
+    def create_and_show_GUI(self):
         """
         Creates and displays a window that allows the user to choose a file.
         """
@@ -14,7 +16,7 @@ class Gui:
         self.window = tk.Tk()
         self.window.title("Squash Analysis")
         select_file_button = tk.Button(self.window, text="Choose File",
-                                       command=self.select_video).place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+                                       command=self.select_video_path).place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
         # Makes the window appear in front of other windows when starting the application
         self.window.lift()
@@ -22,13 +24,13 @@ class Gui:
         self.window.after_idle(self.window.attributes, '-topmost', False)
         self.window.mainloop()
 
-    def select_video(self):
-        """Opens a file choosing dialog, allowing the user to select a video file."""
+    def select_video_path(self):
+        """
+        Opens a file choosing dialog, allowing the user to select a video file.
+        """
+
         self.file_path = filedialog.askopenfilename(
             title="Choose a file",
             filetypes=[("video files", (".mp4", ".mov", ".MOV"))]
         )
         self.window.destroy()
-
-
-
