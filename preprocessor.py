3a08dc6f85bdd862a4f3d4e6e84d45c90b6e20a4
Walter Herold Veedla <walterheroldv@gmail.com>
2021-09-07 09:37:01 +0200

    Basic, slow, medium-success flicker removal added in deflicker at preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import numpy as np
 from collections import deque
 
+from matplotlib import pyplot as plt
+
 
 class Preprocessor:
     """
@@ -34,7 +36,12 @@ class Preprocessor:
         :return: A binary image that differentiates moving parts of the image from static parts from 3 last video frames
         """
         grayscaled = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        if self.deflicker(grayscaled) is not None:
+            grayscaled = self.deflicker(grayscaled)
+
         smoothed = cv.GaussianBlur(grayscaled, (5, 5), 0)
+
         self.frame_buffer.append(smoothed)
 
         if len(self.frame_buffer) < 3:  # Reading initial frames
@@ -49,11 +56,70 @@ class Preprocessor:
         # Combine with boolean "AND"
         combined = cv.bitwise_and(self.frame_difference_buffer[0], self.frame_difference_buffer[1])
 
-        # Threshold to obtain binary image
-        thresholded = cv.threshold(combined, 0, 255, cv.THRESH_OTSU)[1]
+        ret, thresholded = cv.threshold(combined, 0, 255, cv.THRESH_OTSU)
+        thresholded_with_discard = discards_low_intensity_pixels(combined)
 
+        processed = self.morphological_close(thresholded)
+        processed_with_discard = self.morphological_close(thresholded_with_discard)
+
+        # show_histogram(combined, processed)
+        return processed, processed_with_discard
+
+    def morphological_close(self, image):
         # Morphological closing: dilation -> erosion
-        dilated = cv.dilate(thresholded, self.dilation_kernel, iterations=9)  # I played around with this quite a bit and it helped to increase iterations
+        dilated = cv.dilate(image, self.dilation_kernel, iterations=13)
         processed = cv.erode(dilated, self.dilation_kernel)
-
         return processed
+
+    prev_deflicker = None
+    def deflicker(self, frame: np.ndarray, strengthcutoff=20):
+        """Compares the corresponding pixels in the last two frames and
+        if their difference is below a given threshold, it adjusts the intensity of the
+        given pixel in the current frame to be a closer match to the pixel in the previous frame,
+        in essence removing some flickering noise.
+        :return: Frame with adjusted intensities
+        """
+
+        if self.prev_deflicker is None:
+            self.prev_deflicker = frame
+            return
+
+        for row in range(len(frame)):
+            for col in range(len(frame[row])):
+
+                prev_intensity = self.prev_deflicker[row, col]
+                curr_intensity = frame[row, col]
+
+                strength = abs(int(curr_intensity) - int(prev_intensity))
+                #print(f'strength = abs({curr_intensity} - {prev_intensity}) = {strength}')
+
+                # the strength of the stimulus must be greater than a certain point, else we do not want to allow the
+                # change
+                if strength < strengthcutoff:
+                    #print(f'cutoff met: {strength} < {strengthcutoff}')
+                    if curr_intensity > prev_intensity:
+                        frame[row, col] = prev_intensity + 1
+                    else:
+                        frame[row, col] = prev_intensity - 1
+
+        return frame
+
+
+def discards_low_intensity_pixels(frame):
+    # flickering is probably lots of pixels with low intensities
+    # discard pixels with low intensities
+    # preserves higher intensity pixels
+    thresholded1 = cv.threshold(frame, 6, 255, cv.THRESH_TOZERO)[1]
+    retf, thresholded = cv.threshold(thresholded1, 0, 255, cv.THRESH_OTSU)
+    print("retf: " + str(retf))
+    return thresholded
+
+
+def show_histogram(img, processed_img):
+    """Debugging function that shows plotted histogram of two images."""
+    hist_full = cv.calcHist([img], [0], None, [255], [0, 255])
+    plt.subplot(221), plt.imshow(img, 'gray')
+    plt.subplot(222), plt.imshow(processed_img, 'gray')
+    plt.subplot(223), plt.plot(hist_full)
+    plt.xlim([0, 255])
+    plt.show()
