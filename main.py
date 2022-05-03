6b75bb310d887692a7af78d27e12f6edeffa25a4
Walter Herold Veedla <walterheroldv@gmail.com>
2022-05-03 20:28:50 +0200

    Clean up main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
-import sys
 
 import cv2
-
-from Gui import *
-from deflicker import Deflicker
 
 from detector import Detector
 from estimator import Estimator
@@ -12,20 +8,14 @@ from preprocessor import *
 from videoReader import VideoReader
 
 # region video paths
-VIDEO_PATH = "resources/test/test_media_normal.mov"
-VIDEO_PATH1 = "resources/test/720p_solo.mov"
-VIDEO_PATH5 = "resources/test/480p_solo.mov"
-VIDEO_PATH2 = "resources/test/2players.mov"
-VIDEO_PATH3 = "resources/test/short.mov"
+VIDEO_PATH = "resources/test/480p_solo.mov"
 # endregion video paths
 
-video_reader = VideoReader(VIDEO_PATH5)
+video_reader = VideoReader(VIDEO_PATH)
+video_reader = VideoReader("../../Downloads/IMG_4189480.mov")
 preprocessor = Preprocessor()
 estimator = Estimator([0, 0, 0, 0], [0, 0, 0, 0])  # Initially no data about the ball, so initialize with 0-s
 detector = Detector()
-estimator = Estimator()
-
-ball_position_buffer = deque(maxlen=2)
 
 
 def initialize_preprocessor():
@@ -41,52 +31,42 @@ def main():
     # g.create_and_show_GUI()
     # endregion gui
 
+    debug = False
+    cv_frame_wait_time = 1  # 0 for openCV means wait indefinitely
+
     initialize_preprocessor()
-
-
-
-    size = (height * 2, width * 2)  # *2 for adding images side by side
-    fourcc = cv.VideoWriter_fourcc(*'XVID')
-    # out2 = cv.VideoWriter('test5.avi', fourcc, 20.0, size)
 
     for frame in video_reader.get_frame():
         preprocessed = preprocessor.process(frame)
 
-            # TODO currently is min_contour, but should be ball candidate
-            min_contour = min(cleaned_contours, key=rect_area)
-            ball_position_buffer.append(min_contour)
+        prediction = estimator.predict(t=0.25)
+        ball_bounding_box = detector.select_most_probable_candidate(preprocessed, prediction)
+        estimator.add_data(position=ball_bounding_box)
 
-            prediction = None
+        # region drawing
+        draw_contour(frame, ball_bounding_box, (0, 0, 255))
+        if prediction == ball_bounding_box:
+            draw_contour(frame, prediction, (0, 255, 0))
+        img = frame
+        if debug:
+            debug_img = draw_grid(preprocessed)
+            img = np.concatenate((frame, debug_img), axis=1)  # Concatenate along horizontal axis
+        # endregion drawing
 
-            if rect_area(min_contour) < 30000:
-                estimator.add_data(min_contour)
-            else:
-                prediction = estimator.predict(t=0.25)
-                estimator.add_data(prediction)
-            # If estimator has not been initialized, it returns nonsense.
-            if not estimator.initialize_estimator():
-                prediction = None
+        # region keys
+        key = cv2.waitKey(cv_frame_wait_time)
+        if key == ord('q'):
+            break
+        elif key == ord('d'):
+            debug = not debug
+        elif key == ord('w'):
+            cv_frame_wait_time = 0
+        elif key == ord('f'):
+            cv_frame_wait_time = 1
+        # endregion
 
-            contoured_image = draw_contours(frame, preprocessed, detector)
-            img = draw_grid(preprocessed)
-            if prediction is not None:
-                contoured_image = cv.rectangle(contoured_image, (int(prediction[0]), int(prediction[1])),
-                                               (int(prediction[0] + prediction[2]), int(prediction[1] + prediction[3])),
-                                               (0, 255, 255), thickness=3)
-                img = cv.rectangle(img, (int(prediction[0]), int(prediction[1])),
-                                   (int(prediction[0] + prediction[2]), int(prediction[1] + prediction[3])),
-                                   (0, 255, 255), thickness=3)
-
-            # Convert grayscale image to 3-channel image,so that they can be stacked together
-            both = np.concatenate((contoured_image, img), axis=1)  # 1 : horz, 0 : Vert.
-            cv2.imshow('imgc', both)
-
-            # if cv.waitKey() == ord('q'):
-            #     break
-            cv.waitKey(1)
-
-    # out2.release()
-    cv.destroyAllWindows()
+        cv2.imshow("Video", img)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
