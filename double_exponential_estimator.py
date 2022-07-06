bc4bb68f3ae1b5cba18d47b5433f0976f526e627
Walter Herold Veedla <walterheroldv@gmail.com>
2022-07-06 19:06:48 +0300

    Updated list usages to Rect.

--- double_exponential_estimator.py
+++ double_exponential_estimator.py
@@ -2,12 +2,14 @@
 # https://www.itl.nist.gov/div898/handbook/pmc/section4/pmc433.htm
 from collections import deque
 
+from rect import Rect
+
 
 class DoubleExponentialEstimator:
     """Provides an implementation for double exponential smoothing.
     """
 
-    def __init__(self, initial_pos=(0, 0, 0, 0), next_pos=(0, 0, 0, 0)):
+    def __init__(self, initial_pos=Rect(0, 0, 0, 0), next_pos=Rect(0, 0, 0, 0)):
         """Create and initialize the estimator for forecasting.
         :param initial_pos: Initial observation position of Rectangle [top-left x, top-left y, width, height].
         :param next_pos: Next observation position of Rectangle [top-left x, top-left y, width, height].
@@ -17,30 +19,28 @@ class DoubleExponentialEstimator:
 
         self.__position_buffer = deque([initial_pos, next_pos], maxlen=2)
 
-        x0, y0, width0, height0 = self.__position_buffer[0]
-        x1, y1, width1, height1 = self.__position_buffer[1]
+        self.__previous_smoothed = (self.__position_buffer[0].x, self.__position_buffer[0].y)
+        self.__previous_trend = (self.__position_buffer[1].x - self.__position_buffer[0].x,
+                                 self.__position_buffer[1].y - self.__position_buffer[0].y)
 
-        self.__previous_smoothed = (x0, y0)
-        self.__previous_trend = (x1 - x0, y1 - y0)
-
-    def correct(self, position: list) -> None:
+    def correct(self, position: Rect) -> None:
         """Add data to the position buffer.
         :param position: Bounding rectangle [top-left x, top-left y, width, height] of tracked object.
         """
         self.__position_buffer.append(position)
 
-    def predict(self, t=1.0) -> list:
+    def predict(self, t=1.0) -> Rect:
         """Forecasts a Rectangle [top-left x, top-left y, width, height] for time t=X.
         :param t: Time-step for which the forecast is made. Fractional time-steps are supported.
         :return: Predicted future bounding rectangle [top-left x, top-left y, width, height] of tracked object.
         """
-        true_x, true_y, true_width, true_height = self.__position_buffer[-1]
+        prev_pos = self.__position_buffer[-1]
 
         smoothed_previous_x, smoothed_previous_y = self.__previous_smoothed
         trend_previous_x, trend_previous_y = self.__previous_trend
 
-        smoothed_x = self.__calculate_smoothed_value(true_x, smoothed_previous_x, trend_previous_x)
-        smoothed_y = self.__calculate_smoothed_value(true_y, smoothed_previous_y, trend_previous_y)
+        smoothed_x = self.__calculate_smoothed_value(prev_pos.x, smoothed_previous_x, trend_previous_x)
+        smoothed_y = self.__calculate_smoothed_value(prev_pos.y, smoothed_previous_y, trend_previous_y)
 
         trend_x = self.__calculate_trend_estimate(smoothed_x, smoothed_previous_x, trend_previous_x)
         trend_y = self.__calculate_trend_estimate(smoothed_y, smoothed_previous_y, trend_previous_y)
@@ -55,8 +55,8 @@ class DoubleExponentialEstimator:
         self.__previous_smoothed = (smoothed_x, smoothed_y)
         self.__previous_trend = (trend_x, trend_y)
 
-        # Return a "rectangle" [top-left x, top-left y, width, height]
-        return [int(prediction_x), int(prediction_y), true_width, true_height]
+        # Return a Rect(top-left x, top-left y, width, height)
+        return Rect(int(prediction_x), int(prediction_y), prev_pos.width, prev_pos.height)
 
     def __calculate_smoothed_value(self, observed_true: float, prev_smoothed: float, prev_trend: float) -> float:
         """Calculate the 'smoothed value' part of a double-exponential smoothing process.
