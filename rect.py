9a79a823d41ec74df6fe3ed775d5866857691581
Walter Herold Veedla <walterheroldv@gmail.com>
2022-07-06 18:30:42 +0300

    Add rect.py

---
+++ rect.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Rect:
+    """Class for representing a rectangle in (x, y, width, height) form."""
+    x: float
+    y: float
+    width: int
+    height: int
+
+    def center(self) -> (int, int):
+        """
+        :return: Center (x: int, y: int) of rectangle.
+        """
+        return int(self.x + self.width / 2), int(self.y + self.height)
+
+    def center3D(self) -> (int, int, 1):
+        return *self.center(), 1
+
+    def area(self) -> int:
+        """
+        :return: Area of rectangle.
+        """
+        return self.width * self.height
