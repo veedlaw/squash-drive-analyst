dccd9c45606b499b7354d8da74dfe50982f29259
Walter Herold Veedla <walterheroldv@gmail.com>
2022-07-27 00:37:04 +0300

    Update main to use newly added Pipeline

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 
 from gui import file_selection, set_up_view, guistate
 from gui.analysis_view import AnalysisView
+from pipeline import Pipeline
 
 from preprocessor import *
 from double_exponential_estimator import DoubleExponentialEstimator
@@ -34,21 +35,13 @@ class MainApplication(tk.Frame):
         # Go to the file selection view
         self.view = file_selection.FileSelectionView(master)
 
-        # Set up the processing pipeline
+        self.__init_frame = None
         self.video_reader = None  # To-be-selected
-        self.preprocessor = Preprocessor()
-        self.estimator = DoubleExponentialEstimator()
-        self.detector = Detector()
-        self.bounce_detector = BounceDetector(0, 0)  # TODO to-be-selected
-        self.target_rects = create_target_rects()
-        self.stats_tracker = AccuracyStatistics(self.target_rects)
-        self.court_img = Court.get_court_drawing()
-        Court.draw_targets_grid(self.court_img, self.target_rects)
 
-        master.bind(guistate.SETUP, self.__change_state_SETUP)
+        master.bind(guistate.SETUP, self.__try_change_state_SETUP)
         master.bind(guistate.ANALYSIS, self.__change_state_ANALYSIS)
 
-    def __change_state_SETUP(self, evt: tk.Event) -> None:
+    def __try_change_state_SETUP(self, evt: tk.Event) -> None:
         """
         Assumes state change from file selection to set-up state.
         State transition fails in case of I/O errors.
@@ -57,10 +50,9 @@ class MainApplication(tk.Frame):
         if not self.__try_initialize_video_reader(self.view.file_path):
             return
 
-        self.__initialize_preprocessor()
-        self.view = set_up_view.SetUpWindow(root, next(self.video_reader.get_frame()))
+        self.__init_frame = next(self.video_reader.get_frame())
+        self.view = set_up_view.SetUpWindow(root, self.__init_frame)
 
-    # TODO
     def __change_state_ANALYSIS(self, evt: tk.Event) -> None:
         """
         Assumes state change from set-up to analysis state.
@@ -68,8 +60,10 @@ class MainApplication(tk.Frame):
         """
         # Tear down the old frame
         self.view.teardown()
+
+        pipeline = Pipeline(self.video_reader, [0, 0])  # TODO
         # Move into analysis view
-        self.view = AnalysisView(self.__master, next(self.video_reader.get_frame()), self.video_reader, self.court_img)
+        self.view = AnalysisView(self.__master, self.__init_frame, pipeline)
 
     def __try_initialize_video_reader(self, file_path) -> bool:
         """
@@ -83,21 +77,6 @@ class MainApplication(tk.Frame):
         except:
             messagebox.showerror("Error", "An error occurred during file selection.")
             return False
-
-    def __initialize_preprocessor(self) -> None:
-        """
-        Initializes the PreProcessor with starting frames.
-        """
-        for frame in self.video_reader.get_frame():
-            if self.preprocessor.ready():
-                return
-            self.preprocessor.initialize_with(frame)
-
-    # TODO
-    def __initialize_bounce_detector(self):
-        pass
-
-
 
 
 if __name__ == "__main__":
