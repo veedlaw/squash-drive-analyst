c7028c8ca7d78a360bfaa7c907e2e334628e6190
Walter Herold Veedla <walterheroldv@gmail.com>
2022-07-27 16:25:31 +0300

    Move create_target_rects() into court.py

--- stats.py
+++ stats.py
@@ -1,32 +1,8 @@
 from typing import List
 
 from utils import utilities
-from utils.court import Court
 from utils.rect import Rect
 import cv2 as cv
-
-
-def create_target_rects() -> List[Rect]:
-    """
-    TODO
-    :return:
-    """
-
-    dir = 1
-    zone1_R = Rect(x=int(Court.half_court_line_mid_court[0]),
-                   y=Court.short_line_from_front_wall,
-                   width=dir * int(Court.service_box_len * Court.wConv),
-                   height=int(Court.service_box_len * Court.hConv))
-    zone2_R = Rect(zone1_R.x, zone1_R.y + zone1_R.height, zone1_R.width, int((261 / 2) * Court.hConv))
-    zone3_R = Rect(zone1_R.x, zone2_R.y + zone2_R.height, zone2_R.width, int((261 / 2) * Court.hConv) + 8)
-    rects = [zone1_R, zone2_R, zone3_R]
-    widths = [dir * int((Court.service_box_len - 30) * Court.wConv), dir * int(40 * Court.wConv)]
-    for i in range(2):
-        parent_rect = rects[-1]
-        for j in range(3):
-            rects.append(Rect(parent_rect.x + parent_rect.width, rects[j].y, widths[i], rects[j].height))
-
-    return rects
 
 
 class AccuracyStatistics:
