b9f535c90529cae545167787d648d7031d39bd0a
Walter Herold Veedla <walterheroldv@gmail.com>
2022-07-28 13:27:47 +0300

    Implemented Output view data output

--- gui/output_view.py
+++ gui/output_view.py
@@ -1,3 +1,5 @@
+from tkinter.constants import LEFT
+
 import numpy as np
 
 from gui.panel_view import PanelView
@@ -5,9 +7,30 @@ from stats import AccuracyStatistics
 
 
 class OutputView:
+    """
+    Manages the output screen of the application.
+    """
 
     def __init__(self, master, stats: AccuracyStatistics, court: np.ndarray):
 
-        self.__master = master
-        self.__stats_tracker = stats
         self.__view = PanelView(master, court)
+
+        self.__master = master
+        self.__master.title("Processed results")
+        self.__stats_tracker = stats
+        self.__target_rects = stats.get_target_rects()
+        self.__court_img = court
+
+        self.show_stats()
+
+    def show_stats(self) -> None:
+        """
+        Draw the final output screen.
+        """
+        # Clear the right label for text
+        self.__view.label_right.configure(image='')
+        self.__view.label_right.configure(font=("TkDefaultFont", 24), anchor='e', justify=LEFT)
+
+        self.__stats_tracker.draw_box_markings(self.__court_img)
+        self.__view.update_label_left(self.__court_img)
+        self.__view.label_right.configure(text=(self.__stats_tracker.get_result_str_boxwise()))
