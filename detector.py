25813020794b2b308d9a1f295a220361fa2b059c
Walter Herold Veedla <walterheroldv@gmail.com>
2023-01-04 10:38:13 +0100

    REMOVE: Code referencing deflickering

--- detector.py
+++ detector.py
@@ -88,12 +88,6 @@ class Detector:
         :param frame: A video frame.
         """
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # if self.__prev_deflicker is None:
-        #     self.__prev_deflicker = frame
-        # else:
-        #     self.__deflicker(frame)
-
         frame = cv2.GaussianBlur(frame, (5, 5), 0)
 
         self.__frame_buffer.append(frame)
@@ -113,18 +107,3 @@ class Detector:
         dilated = cv2.dilate(image, self.__dilation_kernel, iterations=iterations)
         processed = cv2.erode(dilated, self.__dilation_kernel)
         return processed
-
-    def __deflicker(self, current_frame: np.ndarray, strength_cutoff=25) -> None:
-        """
-        Compares the corresponding pixels in the last two frames and
-        if their difference is below a given threshold, it adjusts the intensity of the
-        given pixel in the current frame to be a closer match to the pixel in the previous frame,
-        in essence removing some flickering noise.
-        """
-
-        strength_change_mask = np.abs(current_frame.astype(np.int16) - self.__prev_deflicker) < strength_cutoff
-        current_frame[strength_change_mask] = self.__prev_deflicker[strength_change_mask] + \
-                                              np.where(np.greater(current_frame[strength_change_mask],
-                                                                  self.__prev_deflicker[strength_change_mask]), 1, -1)
-
-        self.__prev_deflicker = np.copy(current_frame)
